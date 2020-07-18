bed6cd500dd8fae3c6f28ca8648dfea05ca05678
gabs-dev <gabrielfelipe1458@gmail.com>
2020-07-18 19:41:11 -0300

    Cadastrando empresa

--- src/controller/RegisterCompanyController.java
+++ src/controller/RegisterCompanyController.java
@@ -1,17 +1,24 @@
 package controller;
 
+import dao.CompanyDao;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import jdbc.exception.DbException;
+import model.Company;
 import sample.Principal;
 import sample.RegisterCompany;
+import util.Alerts;
+import util.CpfCnpjValidator;
 
 import java.net.URL;
+import java.util.InputMismatchException;
 import java.util.ResourceBundle;
 
 public class RegisterCompanyController implements Initializable {
@@ -45,15 +52,38 @@ public class RegisterCompanyController implements Initializable {
         });
 
         btnRegister.setOnMouseClicked((MouseEvent e) -> {
-
+            registerCompany();
         });
 
         btnRegister.setOnKeyPressed((KeyEvent e) -> {
             if(e.getCode() == KeyCode.ENTER) {
-
+                registerCompany();
             }
         });
 
+    }
+
+    private void registerCompany() {
+        String name = txtName.getText(),
+                email = txtEmail.getText(),
+                cnpj = txtCnpj.getText();
+
+        if (CpfCnpjValidator.isCnpj(cnpj)) {
+            Company c = new Company(name, email, cnpj);
+            CompanyDao dao = new CompanyDao();
+            try {
+                dao.add(c);
+                openPrincipal();
+                Alerts.showAlert("Sucesso", null, "Empresa cadastrada com sucesso", AlertType.INFORMATION);
+                close();
+            } catch (DbException e) {
+                String message = "Erro ao cadastrar empresa";
+                message += "\n" + e.getMessage();
+                Alerts.showAlert("Erro", null, message, AlertType.ERROR);
+            }
+        } else {
+            Alerts.showAlert("Erro", null, "CNPJ inválido", AlertType.ERROR);
+        }
     }
 
     public void close() {
---
+++ src/dao/CompanyDao.java
@@ -0,0 +1,150 @@
+package dao;
+
+import jdbc.ConnectionFactory;
+import jdbc.exception.DbException;
+import model.Company;
+import model.Person;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompanyDao implements IDao<Company> {
+
+    private Connection conn;
+
+    public CompanyDao() {
+        this.conn = ConnectionFactory.getConnection();
+    }
+
+    @Override
+    public void add(Company obj) {
+        PreparedStatement st = null;
+        String sql = "INSERT INTO company (name, email, cnpj) " +
+                "VALUES (?, ?, ?);";
+
+        try {
+            conn.setAutoCommit(false);
+
+            st = conn.prepareStatement(sql);
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setString(3, obj.getCnpj());
+            st.execute();
+
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+                throw new DbException(e1.getMessage());
+            }
+            throw new DbException(e.getMessage());
+        } finally {
+            ConnectionFactory.closeStatement(st);
+            ConnectionFactory.closeConnection();
+        }
+    }
+
+    @Override
+    public void update(Company obj) {
+        PreparedStatement st = null;
+        String sql = "UPDATE company " +
+                "SET name = ?, email = ?, cnpj = ? " +
+                "WHERE id = ?;";
+
+        try {
+            conn.setAutoCommit(false);
+
+            st = conn.prepareStatement(sql);
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setString(3, obj.getCnpj());
+            st.setLong(4, obj.getId());
+            st.execute();
+
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+                throw new DbException(e1.getMessage());
+            }
+            throw new DbException(e.getMessage());
+        } finally {
+            ConnectionFactory.closeStatement(st);
+            ConnectionFactory.closeConnection();
+        }
+    }
+
+    @Override
+    public void delete(Company obj) {
+        PreparedStatement st = null;
+        String sql = "DELETE FROM company WHERE id = ?;";
+
+        try {
+            conn.setAutoCommit(false);
+
+            st = conn.prepareStatement(sql);
+            st.setLong(1, obj.getId());
+            st.execute();
+
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+                throw new DbException(e1.getMessage());
+            }
+            throw new DbException(e.getMessage());
+        } finally {
+            ConnectionFactory.closeStatement(st);
+            ConnectionFactory.closeConnection();
+        }
+    }
+
+    @Override
+    public List<Company> findAll() {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        List<Company> list = new ArrayList<>();
+        String sql = "SELECT * FROM company";
+        try {
+            st = conn.prepareStatement(sql);
+            rs = st.executeQuery();
+
+            while(rs.next()) {
+                Company c = instantiateCompany(rs);
+                list.add(c);
+            }
+
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DbException(e.getMessage());
+        } finally {
+            ConnectionFactory.closeStatement(st);
+            ConnectionFactory.closeResultSet(rs);
+            ConnectionFactory.closeConnection();
+        }
+    }
+
+    private Company instantiateCompany(ResultSet rs) throws SQLException {
+        Company obj = new Company();
+        obj.setId(rs.getLong("id"));
+        obj.setName(rs.getString("name"));
+        obj.setEmail(rs.getString("email"));
+        obj.setCnpj(rs.getString("cnpj"));
+        return obj;
+    }
+
+}
