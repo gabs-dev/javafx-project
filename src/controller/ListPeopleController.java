082e2b11073de00876cc2e5c1ffd2fa6b83bd4bb
gabs-dev <gabrielfelipe1458@gmail.com>
2020-07-20 19:56:21 -0300

    Deletando pessoa listada no tableview

--- src/controller/ListPeopleController.java
+++ src/controller/ListPeopleController.java
@@ -1,14 +1,14 @@
 package controller;
 
 import dao.PersonDao;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import jdbc.exception.DbException;
@@ -19,6 +19,7 @@ import util.Alerts;
 import util.Navigation;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ListPeopleController implements Initializable {
@@ -44,6 +45,8 @@ public class ListPeopleController implements Initializable {
     @FXML
     private Button btnGoBack;
 
+    private Person selected;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initTable();
@@ -51,6 +54,21 @@ public class ListPeopleController implements Initializable {
         btnGoBack.setOnMouseClicked((MouseEvent e) -> {
             Navigation.openScreen(new Principal());
             Navigation.close(ListPeople.getStage());
+        });
+
+        btnDelete.setOnMouseClicked((MouseEvent e) -> {
+            delete();
+        });
+
+        btnUpdate.setOnMouseClicked((MouseEvent e) -> {
+            table.setItems(updateTable());
+        });
+
+        table.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Person>() {
+            @Override
+            public void changed(ObservableValue<? extends Person> observableValue, Person oldValue, Person newValue) {
+                selected = newValue;
+            }
         });
     }
 
@@ -69,6 +87,25 @@ public class ListPeopleController implements Initializable {
             Alerts.showAlert("Erro", "", "Erro ao exibir a lista de pessoas!\n" + e.getMessage(), AlertType.ERROR);
             e.printStackTrace();
             throw new DbException(e.getMessage());
+        }
+    }
+
+    private void delete() {
+        PersonDao dao = new PersonDao();
+        if (selected != null) {
+            try {
+                Optional<ButtonType> result = Alerts.showConfirmation("Confirmação", "Tem certeza que deseja excluir o(a) usuário(a) " + selected.getName());
+                if (result.get() == ButtonType.OK) {
+                    dao.delete(selected);
+                    Alerts.showAlert("Excluído",  null, "Usuário excluído com sucesso!", AlertType.INFORMATION);
+                    table.setItems(updateTable());
+                }
+            } catch (DbException e) {
+                e.printStackTrace();
+                Alerts.showAlert("Erro", null, "Não foi possível excluir a pessoa!\n" + e.getMessage(), AlertType.ERROR);
+            }
+        } else {
+            Alerts.showAlert("Selecione uma pessa", null, "É preciso selecionar uma pessoa para excluir!", AlertType.WARNING);
         }
     }
 
